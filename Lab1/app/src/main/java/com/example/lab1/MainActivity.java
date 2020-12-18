2a8a79ab8e6173e61efdf602ff96baaeb16b4977
HutskoAntonEpam <61325582+HutskoAntonEpam@users.noreply.github.com>
2020-12-18 15:22:52 +0300

    Lab1 mod

---
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.text1);
+        if (getIntent().getStringExtra("number")!=null){
+            tv.setText(getIntent().getStringExtra("number"));
+        }
+
+        EditText edittext = findViewById(R.id.editText);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!edittext.getText().toString().equals("")&&edittext.getText().toString().matches("-?\\d+(\\.\\d+)?")) {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    intent.putExtra("number",
+                            Integer.parseInt(tv.getText().toString())
+                                    + Integer.parseInt(edittext.getText().toString()) + "");
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
